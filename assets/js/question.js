eb27b8c59a5dde21140b77c312bcfcd2860afae6
Steven Woo <swoo9875@gmail.com>
2021-01-14 14:37:33 -0500

    created a template for the page

---
+++ assets/js/question.js
@@ -0,0 +1,54 @@
+var questionlist = [
+    {
+        question: "The first legendary Pokémon, introduced in the Kanto region are Articuno, Zapdos, and",
+        choices: {
+            a: "Mew",
+            b: "Lugia",
+            c: "Victini",
+            d: "Zaptos",
+        },
+        correct: "d"
+    },
+
+    {
+        question: "What's the pre-evolution of Gyrados?",
+        choices: {
+            a: "Magikarp",
+            b: "Feebas",
+            c: "Tynamo",
+            d: "Remoraid",
+        },
+        correct: "a"
+    },
+    {
+        question: "Pikachu is what type of pokemon?",
+        choices: {
+            a: "Water",
+            b: "Electric",
+            c: "Fire",
+            d: "Fairy",
+        },
+        correct: "b"
+    },
+    {
+        question: "Mewtwo is a man-made pokemon created from the DNA of which pokemon?",
+        choices: {
+            a: "Jirachi",
+            b: "Mew",
+            c: "Deoxys",
+            d: "Mespirit",
+        },
+        correct: "b"
+    },
+
+    {
+        question: "What does charmander evolved into?",
+        choices: {
+            a: "Ivysaur",
+            b: "Wartortle",
+            c: "Charmeleon",
+            d: "Marshtomp",
+        },
+        correct: "c"
+    },
+];
